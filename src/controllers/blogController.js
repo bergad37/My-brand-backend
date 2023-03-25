0c36e4ecb1dd2ca6e43b2b6e8eb91f81c9c3c810
bergad37 <bergad37@gmail.com>
2023-03-25 16:24:19 +0200

    Hosted to render

--- src/controllers/blogController.js
+++ src/controllers/blogController.js
@@ -1,124 +1,121 @@
 import Blog from "../model/blog.js";
 //import blogs from "../model/blogs.js"
-class blogController{
+class blogController {
 
 
     //Get all blogs
-    static async getBlogs(req,res){
- try {
+    static async getBlogs(req, res) {
+        try {
 
-    const article=await Blog.find();
-  return  res.status(200).json({
-    message:"The list of blogs",
-        data: article
-    })
- } catch (error) {
-    console.log(error)
-   return res.status(500).json({
-        message: "Server error"
-    })
- }
-
- //Create blog function
-    }
-
-    static async getOneBlog(req,res){
-      try {
-        const {id}=req.params;
-//const blogId=Number(id);
-
-        const blog=await Blog.findOne({_id:id})
-
-        if(!blog){
-            return res.status(404).json({
-                message:"The blog not found",
-            })
-        } else{
+            const article = await Blog.find();
             return res.status(200).json({
-                message:`Blog with id ${id}: `,
-                data: blog
+                message: "The list of blogs",
+                data: article
+            })
+        } catch (error) {
+            console.log(error)
+            return res.status(500).json({
+                message: "Server error"
             })
         }
-      } catch (error) {
-        console.log(error);
-        return res.status(500).json({
-             message: "Server error"
-         })
-      }
+        //Create blog function
     }
 
-static async createBlogs(req,res){
-try {
-    const {title,Author,message,imageUrl}=req.body;
-    console.log(imageUrl);
-    const newBlog=await Blog.create({title,Author,message,imageUrl});
-   
-   return  res.status(201).json({
-        Message: "New blog has been created",
-        data: newBlog
-    })
-} catch (error) {
-    console.log(error);
-    return res.status(500).json({
-        message: "Server error"
-    })
-    
-}
-}
-//update my blog
-static async updateBlogs(req,res){
-    try {
-         const {id}=req.params;
-        const _id=id;
-         // const blogId=Number(id);
-        const {title,message,imageUrl}=req.body;
-        const blogUpdated=await Blog.findByIdAndUpdate(_id,{title,message,imageUrl},{new:true})
-        if (!blogUpdated) {
-           return res.status(500).json({
-                message:`Blog with id ${id} not found`
-            })
-        }else{ 
-            // blogToUpdate.title=title;
-            // blogToUpdate.descripton=description;
-            // blogToUpdate.photo=photo;
+    static async getOneBlog(req, res) {
+        try {
+            const { id } = req.params;
+            //const blogId=Number(id);
 
-           return res.status(200).json({
-                message: `Blog with id ${id} updated`,
-                data: blogUpdated
+            const blog = await Blog.findOne({ _id: id })
+
+            if (!blog) {
+                return res.status(404).json({
+                    message: "The blog not found",
+                })
+            } else {
+                return res.status(200).json({
+                    message: `Blog with id ${id}: `,
+                    data: blog
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: "Server error"
             })
         }
-    } catch (error) {
-        res.status(500).json({
-            message: "Server error"
-        })
     }
-}
-static async deleteBlog(req,res){
-    try {
-         const {id} =req.params;
-       const _id=id;
-       const blogToDelete=await Blog.findByIdAndDelete(_id)
-         //  const blogId=Number(id)
-        //  const index=blogs.findIndex(blog => blog.id === blogId)
-         if(!blogToDelete){
-         return res.status(404).json({
-         message: `Blogs with id ${_id} not found`
-})
-         }
-         else{
-            return res.status(200).json({
-                ok:true,
-                message:`Blog  with id ${_id} is deleted successfully`,
+
+    static async createBlogs(req, res) {
+            try {
+                const { title, Author, message, imageUrl } = req.body;
+                console.log(imageUrl);
+                const newBlog = await Blog.create({ title, Author, message, imageUrl });
+
+                return res.status(201).json({
+                    Message: "New blog has been created",
+                    data: newBlog
+                })
+            } catch (error) {
+                console.log(error);
+                return res.status(500).json({
+                    message: "Server error"
+                })
+
+            }
+        }
+        //update my blog
+    static async updateBlogs(req, res) {
+        try {
+            const { id } = req.params;
+            const _id = id;
+            // const blogId=Number(id);
+            const { title, message, imageUrl } = req.body;
+            const blogUpdated = await Blog.findByIdAndUpdate(_id, { title, message, imageUrl }, { new: true })
+            if (!blogUpdated) {
+                return res.status(500).json({
+                    message: `Blog with id ${id} not found`
+                })
+            } else {
+                // blogToUpdate.title=title;
+                // blogToUpdate.descripton=description;
+                // blogToUpdate.photo=photo;
+
+                return res.status(200).json({
+                    message: `Blog with id ${id} updated`,
+                    data: blogUpdated
+                })
+            }
+        } catch (error) {
+            res.status(500).json({
+                message: "Server error"
             })
-         }
-    } 
-    catch (error) {
-        console.log(error);
-        return res.status(500).json({
-            message:`server error`
-        })
+        }
     }
-}
+    static async deleteBlog(req, res) {
+        try {
+            const { id } = req.params;
+            const _id = id;
+            const blogToDelete = await Blog.findByIdAndDelete(_id)
+                //  const blogId=Number(id)
+                //  const index=blogs.findIndex(blog => blog.id === blogId)
+            if (!blogToDelete) {
+                return res.status(404).json({
+                    message: `Blogs with id ${_id} not found`
+                })
+            } else {
+                return res.status(200).json({
+                    ok: true,
+                    message: `Blog  with id ${_id} is deleted successfully`,
+                })
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500).json({
+                message: `server error`
+            })
+        }
+    }
 }
 
 export default blogController
--- src/controllers/commentController.js
+++ src/controllers/commentController.js
@@ -1,25 +1,25 @@
 import Comment from "../model/contactForm.js"
-class commentController{
-    static async comment(req,res){
-        const {name,email,message}=req.body;
-const messenger=await Comment.create({name,email,message}); 
-res.status(201).json({
-    message:"Message sent successfully",
-    data: messenger
-})
+class commentController {
+    static async comment(req, res) {
+        const { name, email, message } = req.body;
+        const messenger = await Comment.create({ name, email, message });
+        res.status(201).json({
+            message: "Message sent successfully",
+            data: messenger
+        })
     }
-    static async getComments(req,res){
-try {
-    const messengers=await Comment.find();
-    return res.status(200).json({
-        message:"The messages",
-        data: messengers
-    })
-} catch (error) {
-    return res.status(500).json({
-        message:"Server error"
-    })
-}
+    static async getComments(req, res) {
+        try {
+            const messengers = await Comment.find();
+            return res.status(200).json({
+                message: "The messages",
+                data: messengers
+            })
+        } catch (error) {
+            return res.status(500).json({
+                message: "Server error"
+            })
+        }
     }
 }
 
