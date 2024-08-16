adcb9ab9f7b8e4c146e91318eb84ded072b1d4e2
bergad37 <bergad37@gmail.com>
2024-08-16 15:37:57 +0200

    protect routes

--- src/middleware/restricDelete.js
+++ src/middleware/restricDelete.js
@@ -1,42 +1,40 @@
 import jwt from "jsonwebtoken"
 
-const allowTodelete= (req,res,next)=>{
+const allowTodelete = (req, res, next) => {
 
-   try {
-    const authH=req.headers.authorization;
-    
-    //Split the string from the Bearer word and get the second index
-const token=authH.split(" ")[1];
-console.log(token)
-    if(!authH){
-        return res.status(401).json({
-            message:"No token provided"
-        })
+    try {
+        const authH = req.headers.authorization;
+
+        //Split the string from the Bearer word and get the second index
+
+        // console.log(token)
+        if (!authH) {
+            return res.status(401).json({
+                message: "No token provided"
+            })
+        } else {
+            const token = authH.split(" ")[1];
+            const checkToken = jwt.verify(token, process.env.SECRETE_KEY);
+            console.log(checkToken.role);
+            if (!checkToken) {
+                return res.status(401).status.json({
+                    message: "Token is invalid or has expired! Retry "
+                })
+            } else {
+                const { role } = checkToken
+                console.log(checkToken.role);
+                console.log(role)
+                if (role === false) {
+                    return res.status(403).json({
+                        message: "Access denied!, You can only edit a blog"
+                    })
+                } else {
+                    next();
+                }
+            }
+        }
+    } catch (error) {
+        console.log(error)
     }
-    else{
-       const checkToken=jwt.verify(token,process.env.SECRETE_KEY);
-        console.log(checkToken.role);
-if(!checkToken){
-   return res.status(401).status.json({
-        message:"Token is invalid or has expired! Retry "
-    })
-}
-else{
-const {role}=checkToken
-console.log(checkToken.role);
-console.log(role)
- if(role===false){
-   return res.status(403).json({
-    message:"Access denied!, You can only edit a blog"
-   }) 
-}
-else{
-next();
-}
-}
-    }
-   } catch (error) {
-    console.log(error)
-   }
 }
 export default allowTodelete
